9de114f20551737752d2f9621a6cf2b2986f435f
yash1022 <dyashsingh.4@gmail.com>
2024-12-20 19:17:32 +0530

    dashboard

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -5,20 +5,52 @@ function Content() {
     return (
         <div>
             <div className="contentcontainer">
-                <div className="calender">
-                <iframe
-                    src="https://calendar.google.com/calendar/embed?src=sudeepti2006%40gmail.com&ctz=Asia%2FKolkata"
-                    style={{ border: "0" }}
-                    width="300"
-                    height="200"
-                    frameBorder="0"
-                    scrolling="no"
-                    title="Google Calendar"
-                    float="right"
-                ></iframe>
-            </div>
-            </div>
-        </div>
+                <div className="upper-tab">
+
+                      <div className="name">
+                        Welcome,John
+                      </div>
+
+                      <div className="time">
+                        Wednesday,26 jan 2024
+                      </div>
+
+                </div>
+
+                <div className="lower-tab1">
+
+                    <div className=" b b1">
+
+                    </div>
+
+                    <div className=" b b2">
+                        
+                    </div>
+
+                    <div className=" b b3">
+
+                        <div className="calender">
+                         <iframe
+                           src="https://calendar.google.com/calendar/embed?src=sudeepti2006%40gmail.com&ctz=Asia%2FKolkata"
+                           style={{ border: "0" }}
+                           width="350"
+                           height="300"
+                           frameBorder="0"
+                           scrolling="no"
+                           title="Google Calendar"
+                           
+                         ></iframe>
+                       </div>
+                        
+                    </div>
+                    
+                </div>
+
+
+               
+         </div>
+    </div>
+        
     );
 }
 
